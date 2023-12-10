425b3a4a3f4a5a7e5d762d441249a16f7b05b3e5
soohocoding <ckdgns1447@gmail.com>
2023-12-10 22:58:25 +0900

    start

---
+++ src/main/java/com/codebee/StyleHive/StyleHiveApplication.java
@@ -0,0 +1,13 @@
+package com.codebee.StyleHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebee/StyleHive/StyleHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebee.StyleHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
